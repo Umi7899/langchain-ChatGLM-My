d2328c680d0f5c4b3d2058b2019c062ee731377e
ZRH <543135875@qq.com>
2023-07-06 10:10:56 +0800

    4次修改：检查相关性匹配

--- chains/local_doc_qa.py
+++ chains/local_doc_qa.py
@@ -105,6 +105,9 @@ def generate_prompt(related_docs: List[str],
                     prompt_template: str = PROMPT_TEMPLATE, ) -> str:
     context = "\n".join([doc.page_content for doc in related_docs])
     prompt = prompt_template.replace("{question}", query).replace("{context}", context)
+    print("______________")
+    print(prompt)
+    print("______________")
     return prompt
 
 
